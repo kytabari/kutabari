23abdf4be41590ad03d134c5f81e7d8710223f85
kytabari <Vektorvr3@gmail.com>
2024-10-03 15:58:29 +0000

    Лекция 1

---
+++ 2.py
@@ -0,0 +1,9 @@
+type # Команда определяет типы данных
+
+type(3)
+print(type(3)) #целые int
+print (type(3.4)) #не целые float
+print(type("Молодец"))
+print(type(False))
+print(type(True))
+print(type(None))
---
+++ zadacha.py
@@ -0,0 +1,16 @@
+print('Hello, World!')
+x='Hello, World!'
+print(type(x))
+print('------------------')
+print('Маша + Петя = Любовь')
+x='Маша + Петя = Любовь'
+print(type(x))
+print('------------------')
+
+print('3 + 4 =')
+x=(3 + 4)
+print(x)
+print(type(x))
+
+
+
