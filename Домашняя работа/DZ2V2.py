984e9ff929634c33683f61811faba8d84608628e
kytabari <Vektorvr3@gmail.com>
2024-10-04 23:07:14 +0000

    Лекция 1

---
+++ Домашняя работа/DZ2V2.py
@@ -0,0 +1,14 @@
+# Через добавление к списку
+qq = []
+a = input('Введите свой возраст:')
+qq.append(a)
+b = input('Введите свой пол(М/Ж):')
+qq.append(b)
+c = input('Введите своё имя:')
+qq.append(c)
+d = input('Введите свой город:')
+qq.append(d)
+e = input('Введите цифру и букву своего клсса:')
+qq.append(e)
+print('')
+print(qq)
